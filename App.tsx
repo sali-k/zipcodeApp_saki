3093a7786f45c640e555a356c8fcaab18d0b0412
sali-k <saki.kimura78@gmail.com>
2020-12-01 14:16:58 +0900

    modify .gitignore

--- App.tsx
+++ App.tsx
@@ -1,12 +1,28 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TextInput, Pressable } from "react-native";
 
 export default function App() {
+  const [value, setValue] = React.useState("");
+  const search = () => {};
   return (
     <View style={styles.container}>
-      <Text>first commit!!</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={(text) => setValue(text)}
+        value={value}
+      />
+      <Pressable
+        style={styles.btn}
+        onPress={(search) => {
+          search;
+        }}
+      >
+        {/* <Pressable style={styles.btn} onPress={() => {}}> */}
+        <Text style={styles.btnText}>住所を取得</Text>
+      </Pressable>
       <StatusBar style="auto" />
+      <Text style={styles.text}></Text>
     </View>
   );
 }
@@ -14,8 +30,32 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#555555",
     alignItems: "center",
     justifyContent: "center",
+  },
+  input: {
+    backgroundColor: "#fff",
+    width: 100,
+    height: 30,
+    borderWidth: 2,
+  },
+  btn: {
+    borderRadius: 7,
+    width: 100,
+    height: 30,
+    backgroundColor: "#7B68EE",
+    borderWidth: 2,
+  },
+  btnText: {
+    padding: "5%",
+    textAlign: "center",
+    fontSize: 18,
+  },
+  text: {
+    backgroundColor: "#fff",
+    width: "80%",
+    height: "50%",
+    borderWidth: 2,
   },
 });
